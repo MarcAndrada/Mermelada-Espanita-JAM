1ead9598e21fcf9c0b9794cf16e8807c5316f55b
Crami <crami.andrada@gmail.com>
2022-12-18 03:42:53 +0100

    Los fachas estan altoke

--- Assets/Scripts/Enemies/EnemyBehaviourController.cs
+++ Assets/Scripts/Enemies/EnemyBehaviourController.cs
@@ -1,30 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviourController : MonoBehaviour
 {
-    public enum EnemyState { WAITING, PATROLING, CHASING, ATTACKING };
+    public enum EnemyState { WAITING, PATROLING, CHASING, ATTACKING, DEAD };
     public EnemyState currentState;
 
-    private EnemyMovementController movementController;
-
-    [Header("DetectionVariables"), SerializeField]
+    [Header("Detection Variables"), SerializeField]
     private float checkRange;
     [SerializeField]
     private float checkDot;
+    [SerializeField]
+    private LayerMask scenariMask;
+
+    [Header("Attack Variables"), SerializeField]
+    
+    
+    private NavMeshAgent agent;
+
 
     private void Awake()
     {
-        movementController = GetComponent<EnemyMovementController>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        StatesFunctions();
+    }
+
+    private void StatesFunctions() 
+    {
+        switch (currentState)
+        {
+            case EnemyState.WAITING:
+                CheckPlayerDotProduct();
+                break;
+            case EnemyState.PATROLING:
+                CheckPlayerDotProduct();
+                break;
+            case EnemyState.CHASING:
+                //Seguir al jugador
+                ChasePlayer();
+                //Mirar a la direccion en la que va 
+                LookForward();
+                //Comprobar la distancia entre el y el player
+                break;
+            case EnemyState.ATTACKING:
+                //Mirar al player
+                LookAtPlayer();
+                //Dar vueltas alrededor del player
 
 
+
+                break;
+            default:
+                break;
+        }
 
     }
 
@@ -32,22 +70,66 @@ public class EnemyBehaviourController : MonoBehaviour
     #region Detection Functions
     private void CheckPlayerDotProduct()
     {
-        Vector3 playerDir = Vector3.zero;
-
-        float dot = Vector2.Dot(transform.forward, playerDir.normalized);
-
-        if (true)
-        {
-
-        }
 
     }
 
-    private void CheckIfSeePlayers() 
+    private void SeePlayer() 
     {
-        
+
+    }
+
+
+    #endregion
+
+    #region Patrol Functions
+
+    #endregion
+
+    #region Chase Functions
+    private void ChasePlayer() 
+    {
+        agent.SetDestination(EnemiesManager._instance.playerRef.transform.position);
+        Debug.Log("Aqui voy :D");
+    }
+    private void LookForward() 
+    {
+        float angle = Mathf.Atan2(agent.velocity.y, agent.velocity.x) * Mathf.Rad2Deg;
+
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+
+        transform.rotation = rotation;
+
+        Debug.Log("GIRASIOOOOON");
     }
 
     #endregion
+
+
+    #region AttackFunctions
+
+    private void LookAtPlayer() 
+    {
+
+
+        Vector2 lookAtDir = EnemiesManager._instance.playerRef.transform.position - transform.position;
+
+        float angle = Mathf.Atan2(lookAtDir.y, lookAtDir.x) * Mathf.Rad2Deg;
+
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+
+        transform.rotation = rotation;
+
+        Debug.Log("GIRASIOOOOON");
+    }
+
+    #endregion
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position,1);
+
+    }
 
 }
