23308fc031e1b0d621ddd514e1248eab7c5b75e0
Crami <crami.andrada@gmail.com>
2022-12-18 12:21:56 +0100

    IA Enemigos practicamente al toke

--- Assets/Scripts/Enemies/EnemyBehaviourController.cs
+++ Assets/Scripts/Enemies/EnemyBehaviourController.cs
@@ -9,15 +9,23 @@ public class EnemyBehaviourController : MonoBehaviour
     public EnemyState currentState;
 
     [Header("Detection Variables"), SerializeField]
-    private float checkRange;
+    private float maxVisionDistance;
     [SerializeField]
     private float checkDot;
     [SerializeField]
     private LayerMask scenariMask;
+    Vector2 rayDir;
 
     [Header("Attack Variables"), SerializeField]
-    
-    
+    private float orbitMoveSpeed;
+    [SerializeField]
+    private float distanceToAttack;
+    private float orbitAccumulator = 0;
+    [SerializeField]
+    private float orbitTargetRotationsSpeed;
+    Vector2 posToOrbit = Vector2.zero;
+
+
     private NavMeshAgent agent;
 
 
@@ -35,15 +43,15 @@ public class EnemyBehaviourController : MonoBehaviour
         StatesFunctions();
     }
 
-    private void StatesFunctions() 
+    private void StatesFunctions()
     {
         switch (currentState)
         {
             case EnemyState.WAITING:
-                CheckPlayerDotProduct();
+                CheckIfPlayerNear();
                 break;
             case EnemyState.PATROLING:
-                CheckPlayerDotProduct();
+                CheckIfPlayerNear();
                 break;
             case EnemyState.CHASING:
                 //Seguir al jugador
@@ -51,12 +59,13 @@ public class EnemyBehaviourController : MonoBehaviour
                 //Mirar a la direccion en la que va 
                 LookForward();
                 //Comprobar la distancia entre el y el player
+                CheckDistanceBtwPlayer();
                 break;
             case EnemyState.ATTACKING:
                 //Mirar al player
                 LookAtPlayer();
                 //Dar vueltas alrededor del player
-
+                OrbitPlayer();
 
 
                 break;
@@ -68,13 +77,50 @@ public class EnemyBehaviourController : MonoBehaviour
 
 
     #region Detection Functions
+    private void CheckIfPlayerNear() 
+    {
+        float distanceBtwPlayer = Vector2.Distance(transform.position, EnemiesManager._instance.playerRef.transform.position);
+
+        if (distanceBtwPlayer < maxVisionDistance) 
+        {
+            //El Player esta lo suficientemente cerca como para mirar donde esta
+            CheckPlayerDotProduct();
+        }
+
+    }
+
     private void CheckPlayerDotProduct()
     {
+        rayDir = EnemiesManager._instance.playerRef.transform.position - transform.position;
 
+        float dot = Vector2.Dot(transform.up, rayDir.normalized);
+        if (dot >= checkDot)
+        {
+            //Comprobar si hay algun muro delante
+            CheckWallsBetween();
+        }
+
+
+    }
+
+
+    private void CheckWallsBetween() 
+    {
+
+        rayDir = EnemiesManager._instance.playerRef.transform.position - transform.position;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDir, Vector2.Distance(transform.position, EnemiesManager._instance.playerRef.transform.position), scenariMask);
+
+        if (hit.collider == null)
+        {
+            SeePlayer();
+        }
     }
 
     private void SeePlayer() 
     {
+        currentState = EnemyState.CHASING;
+        agent.speed = orbitMoveSpeed;
 
     }
 
@@ -99,7 +145,14 @@ public class EnemyBehaviourController : MonoBehaviour
 
         transform.rotation = rotation;
 
-        Debug.Log("GIRASIOOOOON");
+    }
+
+    private void CheckDistanceBtwPlayer() 
+    {
+        if (Vector2.Distance(transform.position, EnemiesManager._instance.playerRef.transform.position) <= distanceToAttack)
+        {
+            currentState = EnemyState.ATTACKING;
+        }
     }
 
     #endregion
@@ -121,14 +174,43 @@ public class EnemyBehaviourController : MonoBehaviour
 
         Debug.Log("GIRASIOOOOON");
     }
+    private void OrbitPlayer() 
+    {
+        orbitAccumulator += Time.fixedDeltaTime * orbitTargetRotationsSpeed;
+        posToOrbit = new Vector2(Mathf.Cos(orbitAccumulator), Mathf.Sin(orbitAccumulator)) * (distanceToAttack - 1.5f);
+        agent.SetDestination(posToOrbit);
+
+        Debug.Log("GIRASIOOOOON");
+
+    }
 
     #endregion
 
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position,1);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, maxVisionDistance);
+
+        if (EnemiesManager._instance)
+        {
+            rayDir = EnemiesManager._instance.playerRef.transform.position - transform.position;
+            float dot = Vector2.Dot(transform.up, rayDir.normalized);
+
+            if (dot >= checkDot)
+            {
+                Gizmos.color = Color.green;
+            }
+            else
+            {
+                Gizmos.color = Color.red;
+            }
+
+
+            Gizmos.DrawRay(transform.position, rayDir);
+        }
+        
 
     }
 
