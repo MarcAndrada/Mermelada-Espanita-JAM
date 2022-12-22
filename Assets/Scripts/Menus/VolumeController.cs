38b4a04b8b3e842c162e1f575785b9c0d87f39c0
Dani <daniel.munoz.jimenez@enti.cat>
2022-12-22 22:37:52 +0100

    Menu principal hacido

--- Assets/Scripts/Menus/VolumeController.cs
+++ Assets/Scripts/Menus/VolumeController.cs
@@ -7,13 +7,11 @@ public class VolumeController : MonoBehaviour
 {
     public Slider slider;
     public float sliderValue;
-    public Image imagenMute;
+
     // Start is called before the first frame update
     void Start()
     {
-        slider.value = PlayerPrefs.GetFloat("volumenAudio", 0.5f);
         AudioListener.volume = slider.value;
-        RevisarSiEstoyMute();
     }
 
     public void ChangeSlider(float valor)
@@ -21,23 +19,5 @@ public class VolumeController : MonoBehaviour
         slider.value = valor;
         PlayerPrefs.SetFloat("volumenAudio", sliderValue);
         AudioListener.volume = slider.value;
-        RevisarSiEstoyMute();
-    }
-    public void RevisarSiEstoyMute()
-    {
-        if (slider.value == 0)
-        {
-            imagenMute.enabled = true;
-        }
-        else
-        {
-            imagenMute.enabled = false;
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
